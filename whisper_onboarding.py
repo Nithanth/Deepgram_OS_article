f0e11da44868c1fc13543632c0819c341aa349d2
Nithanth Ram <nithanth.ram@gmail.com>
2023-12-13 17:04:03 -0600

    added native OpenAI client whisper calling

--- whisper_onboarding.py
+++ whisper_onboarding.py
@@ -1,5 +1,7 @@
 import whisper
 import json 
+import openai
+from openai import OpenAI
 
 def transcribe_audio(model, audio_path):
     """
@@ -14,8 +16,22 @@ def transcribe_audio(model, audio_path):
 
     return result
 
-def main():
+def transcribe_audio_native(model, audio_path):
+    client = OpenAI()
 
+    audio_file = open(audio_path, "rb")
+    transcription = client.audio.transcriptions.create(
+        model=model, 
+        file=audio_file, 
+        response_format="text"
+    )
+
+    with open('transcription_results_whisper_native.json', 'w') as json_file:
+        json.dump(transcription, json_file, indent=4)
+    
+    print(transcription["text"])
+
+def main():
     # Download and load the Whisper model
     model = whisper.load_model("base")
     # The path to audio file
